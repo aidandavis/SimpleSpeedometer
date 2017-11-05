bef5da791ae96aeab0fb0c91e7b750da7b12fe31
Aidan Davis <aidan.davis94@gmail.com>
2017-11-05 21:59:15 +0800

    begining to wire up to speed tracking class

--- app/src/main/java/com/domain/aidandavis/simplespeedo/MainActivity.java
+++ app/src/main/java/com/domain/aidandavis/simplespeedo/MainActivity.java
@@ -1,13 +1,13 @@
 package com.domain.aidandavis.simplespeedo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView speedText;
+    SpeedTracker speedTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +15,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.toolbar_title);
+
+        speedText = (TextView) findViewById(R.id.speedDisplay);
+        speedTracker = new SpeedTracker(getBaseContext());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+        speedTracker.startTracking();
+        startUpdatingDisplay();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        speedTracker.stopTracking();
+        stopUpdatingDisplay();
+    }
+
+    private void startUpdatingDisplay() {
+
+    }
+
+    private void stopUpdatingDisplay() {
+        
     }
 }
